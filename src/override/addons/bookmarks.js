649eff5c3d30dbc80ebfa5a7ea3fe257410a4924
surajk95 <surajkukati95@gmail.com>
2021-08-25 03:09:45 +0530

    refactored bookmarks code

--- src/override/addons/bookmarks.js
+++ src/override/addons/bookmarks.js
@@ -65,11 +65,9 @@ function searchBookmarks(event, bookmarks) {
 
         searchResults.forEach(item => {
             let result = createElement("", "searchResult", "#searchResultsContainer")
-            let resultTitle = document.createElement("div")
-            resultTitle.className = "resultTitle"
+            let resultTitle = createElement("", "resultTitle")
             resultTitle.innerHTML = trimText(item.title, 30)
-            let resultLink = document.createElement("div")
-            resultLink.className = "resultLink"
+            let resultLink = createElement("", "resultLink")
             resultLink.innerHTML = trimText(item.url, 50)
             result.appendChild(resultTitle)
             result.appendChild(resultLink)
@@ -79,174 +77,164 @@ function searchBookmarks(event, bookmarks) {
     }
 }
 
-
 function clickSearchResult(event, item) {
-    const searchResults = document.getElementsByClassName("searchResult");
+    const searchResults = document.getElementsByClassName("searchResult")
     for(let i=0; i<searchResults.length; i++) {
-        searchResults[i].classList.remove("active");
+        searchResults[i].classList.remove("active")
     }
-    event.target.classList.add("active");
-    updateStatusBar(item);
+    event.target.classList.add("active")
+    updateStatusBar(item)
 }
 
 function recursiveSearch(list, bookmarks, value) {
     if(!bookmarks || (!bookmarks.url && (!bookmarks.children || bookmarks.children.length === 0))) {
-        return list;
+        return list
     }
     if(bookmarks.url) {
         if(bookmarks.url.toLowerCase().includes(value) || bookmarks.title.toLowerCase().includes(value))
-            return [...list, { title: bookmarks.title, url: bookmarks.url }];
+            return [...list, { title: bookmarks.title, url: bookmarks.url }]
         else
-            return list;
+            return list
     }
     else {
-        let newList= [];
+        let newList= []
         bookmarks.children.forEach(item => {
-            newList = recursiveSearch(newList, item, value);
-        });
-        return [...list, ...newList];
+            newList = recursiveSearch(newList, item, value)
+        })
+        return [...list, ...newList]
     }
 }
 
 function populateBookmarks(level, bookmarks) {
     for(let i=level; i<level+10; i++) {
-        let oldBookmarksLevel = document.getElementById(`bookmarkLevel-${i}`);
+        let oldBookmarksLevel = document.getElementById(`bookmarkLevel-${i}`)
         if(oldBookmarksLevel)
-            oldBookmarksLevel.remove();
+            oldBookmarksLevel.remove()
     }
     if(!bookmarks || bookmarks.length === 0)
-        return;
+        return
 
-    let bookmarkLevel = document.createElement("div");
-    bookmarkLevel.id = `bookmarkLevel-${level}`;
-    bookmarkLevel.className = "bookmarkLevel";
-    document.getElementById("bookmarksContainer").appendChild(bookmarkLevel);
+    createElement(`bookmarkLevel-${level}`, "bookmarkLevel", "#bookmarksContainer")
     
     bookmarks.forEach((item, index) => {
-        let bookmarkItem = document.createElement("div");
-        bookmarkItem.id = `bookmark-${item.title}`;
-        bookmarkItem.className = `bookmarkItem ${item.url ? `link` : `folder`}`;
+        let bookmarkItem = createElement(
+            `bookmark-${item.title}`,
+            `bookmarkItem ${item.url ? `link` : `folder`}`,
+            `#bookmarkLevel-${level}`
+        )
         
-        let connectorLeft = document.createElement("div");
-        connectorLeft.className = `connectorLeft connector-to-${level}`;
-        bookmarkItem.appendChild(connectorLeft);
-        document.getElementById(`bookmarkLevel-${level}`).appendChild(bookmarkItem);
+        let connectorLeft = createElement("", `connectorLeft connector-to-${level}`)
+        bookmarkItem.appendChild(connectorLeft)
 
-        let title = document.createElement("div");
-        title.className = `itemTitle itemTitle-${level}`;
-        title.innerHTML = item.url ? trimText(item.title, 90) : trimText(item.title, 30);
-        bookmarkItem.appendChild(title);
+        let title = createElement("", `itemTitle itemTitle-${level}`)
+        title.innerHTML = item.url ? trimText(item.title, 90) : trimText(item.title, 30)
+        bookmarkItem.appendChild(title)
 
-        let pinIcon = document.createElement("img");
-        pinIcon.className = `pinIcon`;
-        pinIcon.src = 'assets/pin.svg';
-        pinIcon.title = 'pin bookmark';
-        bookmarkItem.appendChild(pinIcon);
+        let pinIcon = document.createElement("img")
+        pinIcon.className = `pinIcon`
+        pinIcon.src = 'assets/pin.svg'
+        pinIcon.title = 'pin bookmark'
+        bookmarkItem.appendChild(pinIcon)
 
-        let connectorRight = document.createElement("div");
-        connectorRight.className = `connectorRight connector-from-${level}`;
+        let connectorRight = createElement("", `connectorRight connector-from-${level}`)
         bookmarkItem.appendChild(connectorRight);
-        document.getElementById(`bookmarkLevel-${level}`).appendChild(bookmarkItem);
 
-        let tooltip = document.createElement('div');
-        tooltip.classList.add('tooltip');
+        let tooltip = document.createElement('div')
+        tooltip.classList.add('tooltip')
         tooltip.innerHTML = item.url ? `Click to select,<br/>Double click to open link`
-                                        : `Click to select,<br/>Double click to open all links,<br/>Right click to open random`;
-        bookmarkItem.appendChild(tooltip);
+                                        : `Click to select,<br/>Double click to open all links,<br/>Right click to open random`
+        bookmarkItem.appendChild(tooltip)
 
-        bookmarkItem.addEventListener('click', (event)=>selectBookmark(event, level, item, index, true), false);
-        bookmarkItem.addEventListener('dblclick', ()=>openLinks(item), false);
-        bookmarkItem.addEventListener('contextmenu', (event)=>openRandomLink(event, item), false);
-        pinIcon.addEventListener('click', (e) => {e.stopPropagation(); updatePinnedItems('add', item)});
+        bookmarkItem.addEventListener('click', (event)=>selectBookmark(event, level, item, index, true), false)
+        bookmarkItem.addEventListener('dblclick', ()=>openLinks(item), false)
+        bookmarkItem.addEventListener('contextmenu', (event)=>openRandomLink(event, item), false)
+        pinIcon.addEventListener('click', (e) => {e.stopPropagation(); updatePinnedItems('add', item)})
     });
 }
 
 function trimText(text, limit) {
-    return text.length > limit ? text.substring(0, limit) + '...' : text;
+    return (text.length > limit) ? text.substring(0, limit) + '...' : text
 }
 
 function selectBookmark(event, level, item, index, shouldOpenNextLevel) {
     if(shouldOpenNextLevel) {
-        populateBookmarks(level+1, item.children);
-        updateStatusBar(item);
+        populateBookmarks(level+1, item.children)
+        updateStatusBar(item)
     }
 
-    let rightConnectors = document.getElementsByClassName(`connector-from-${level}`);
+    let rightConnectors = document.getElementsByClassName(`connector-from-${level}`)
     for (let i = 0; i < rightConnectors.length; i++) {
-        rightConnectors[i].classList.remove('active');
+        rightConnectors[i].classList.remove('active')
     }
-    let leftConnectors = document.getElementsByClassName(`connector-to-${level+1}`);
+    let leftConnectors = document.getElementsByClassName(`connector-to-${level+1}`)
     for (let i = 0; i < leftConnectors.length; i++) {
         if(leftConnectors[i])
-            leftConnectors[i].classList.add('active');
+            leftConnectors[i].classList.add('active')
     }
 
-    let titles = document.getElementsByClassName(`itemTitle-${level}`);
+    let titles = document.getElementsByClassName(`itemTitle-${level}`)
     for (let i = 0; i < titles.length; i++) {
-        titles[i].classList.remove('active');
+        titles[i].classList.remove('active')
     }
     if(shouldOpenNextLevel)
-        event.target.classList.add('active');
+        event.target.classList.add('active')
     if(!item.url)
-        event.target.parentElement.querySelector('.connectorRight').classList.add('active');
+        event.target.parentElement.querySelector('.connectorRight').classList.add('active')
 
     
-    let connectorVertical = document.createElement("div");
-    connectorVertical.className = `connectorVertical connector-on-${level}`;
-    const connectorHeight = item.children ? (index < item.children.length ? `${item.children.length*90 - 90}px` : '100%') : `0`;
-    connectorVertical.style.height = connectorHeight;
-    const nextLevel = document.getElementById(`bookmarkLevel-${level+1}`);
+    let connectorVertical = document.createElement("div")
+    connectorVertical.className = `connectorVertical connector-on-${level}`
+    const connectorHeight = item.children ? (index < item.children.length ? `${item.children.length*90 - 90}px` : '100%') : `0`
+    connectorVertical.style.height = connectorHeight
+    const nextLevel = document.getElementById(`bookmarkLevel-${level+1}`)
     if(nextLevel)
-        nextLevel.appendChild(connectorVertical);
+        nextLevel.appendChild(connectorVertical)
 }
 
 function updateStatusBar(item) {
-    let oldStatusBar = document.getElementById("bookmarksStatusBar");
+    let oldStatusBar = document.getElementById("bookmarksStatusBar")
     if(oldStatusBar)
-        oldStatusBar.remove();
+        oldStatusBar.remove()
 
-    let itemDescription = document.createElement("div");
-    let heading = document.createElement("div");
-    let link = document.createElement("div");
-    heading.className = "descriptionHeading";
-    link.className="descriptionLink";
-    itemDescription.className = "itemDescription";
+    let itemDescription = document.createElement("div")
+    let heading = document.createElement("div")
+    let link = document.createElement("div")
+    heading.className = "descriptionHeading"
+    link.className="descriptionLink"
+    itemDescription.className = "itemDescription"
     if(item.url) {
-        heading.innerHTML = trimText(item.title, 200);
-        link.innerHTML = trimText(item.url, 300);
-        itemDescription.appendChild(heading);
-        itemDescription.appendChild(link);
+        heading.innerHTML = trimText(item.title, 200)
+        link.innerHTML = trimText(item.url, 300)
+        itemDescription.appendChild(heading)
+        itemDescription.appendChild(link)
     }
     else {
-        itemDescription.innerHTML = trimText(item.title, 90);
+        itemDescription.innerHTML = trimText(item.title, 90)
     }
 
-    const bookmarksStatusBar = document.createElement("div");
-    bookmarksStatusBar.id = "bookmarksStatusBar";
-    bookmarksStatusBar.classList = "active";
-    bookmarksStatusBar.appendChild(itemDescription);
-    document.getElementById("bookmarksContainer").appendChild(bookmarksStatusBar);
+    const bookmarksStatusBar = createElement("bookmarksStatusBar", "active", "#bookmarksContainer")
+    bookmarksStatusBar.appendChild(itemDescription)
 }
 
 function openLinks(item) {
     if(item.url)
-        window.open(item.url, "_blank");
+        window.open(item.url, "_blank")
     else
-        item.children.forEach(child => openLinks(child));
+        item.children.forEach(child => openLinks(child))
 }
 
 function openRandomLink(event, item) {
-    event.preventDefault();
+    event.preventDefault()
     if(item.url)
-        return;
+        return
     else {
-        let selected={}, counter = 50;
+        let selected={}, counter = 50
         do {
-            selected = item.children[Math.floor(Math.random()*(item.children.length - 1))];
-            counter--;
-        } while(!selected.url && counter>=0);
+            selected = item.children[Math.floor(Math.random()*(item.children.length - 1))]
+            counter--
+        } while(!selected.url && counter>=0)
         if(!selected.url)
-            return;
-        window.open(selected.url);
+            return
+        window.open(selected.url)
     }     
 }
