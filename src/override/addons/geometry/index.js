2adddf7c0626e46ba3714e740af3982063939f0b
surajk95 <surajkukati95@gmail.com>
2022-09-01 17:24:38 +0530

    geometry mode updates

--- src/override/addons/geometry/index.js
+++ src/override/addons/geometry/index.js
@@ -8,7 +8,7 @@ export function loadGeometryTheme() {
 }
 
 function loadCss(theme) {
-    loadStyle(`addons/vanilla-themes/${theme}.css`)
+    loadStyle(`addons/geometry/${theme}.css`)
     .then(() => {
         document.querySelector("#seshParent").className = `theme-${theme}`
         initGeometry()
@@ -23,29 +23,73 @@ const themes = [
 
 function initGeometry() {
     console.log(`init geometry`)
-    const num = Math.floor(Math.random()*5) + 5
+    const num = Math.floor(Math.random()*2) + 3
     for(let i=0; i<num; i++) {
         makeSquare()
+        makeCircle()
+        makeTriangle()
     }
+}
+
+function makeTriangle() {
+    const windowHeight = window.innerHeight, windowWidth = window.innerWidth
+    const square = createElement("square","", "#geometryContainer")
+    const size = Math.floor(Math.random()*50) + 100
+    square.style.transform = `rotate(${Math.floor(Math.random()*360)}deg)`
+    square.style.width = `${size}px`
+    square.style.height = `${size}px`
+    square.style.position = 'absolute'
+    square.style.left = `${Math.floor(Math.random()*(windowWidth-200))}px`
+    square.style.top = `${Math.floor(Math.random()*(windowHeight-200))}px`
+    square.innerHTML = `<svg width="${size}" height="${size}" class="shape speed${Math.floor(Math.random()*3) + 2}">
+    <defs>
+        <linearGradient id="grad3" x1="0%" y1="0%" x2="100%" y2="0%">
+        <stop offset="0%" style="stop-color: #322991;stop-opacity:1" />
+        <stop offset="100%" style="stop-color: #6827C8;stop-opacity:1" />
+        </linearGradient>
+    </defs>
+    <polygon points="0,0 ${size/2},${size} ${size},0" fill="url(#grad3)" />
+</svg>`
+}
+
+function makeCircle() {
+    const windowHeight = window.innerHeight, windowWidth = window.innerWidth
+    const square = createElement("square","", "#geometryContainer")
+    const size = Math.floor(Math.random()*50) + 100
+    square.style.transform = `rotate(${Math.floor(Math.random()*360)}deg)`
+    square.style.width = `${size}px`
+    square.style.height = `${size}px`
+    square.style.position = 'absolute'
+    square.style.left = `${Math.floor(Math.random()*(windowWidth-200))}px`
+    square.style.top = `${Math.floor(Math.random()*(windowHeight-200))}px`
+    square.innerHTML = `<svg width="${size}" height="${size}" class="shape speed${Math.floor(Math.random()*3) + 2}">
+    <defs>
+        <linearGradient id="grad3" x1="0%" y1="0%" x2="100%" y2="0%">
+        <stop offset="0%" style="stop-color: #322991;stop-opacity:1" />
+        <stop offset="100%" style="stop-color: #6827C8;stop-opacity:1" />
+        </linearGradient>
+    </defs>
+  <circle cx="${size/2}" cy="${size/2}" r="${size/2}" stroke="black" stroke-width="0" fill="url(#grad3)" />
+</svg>`
 }
 
 function makeSquare() {
     const windowHeight = window.innerHeight, windowWidth = window.innerWidth
     const square = createElement("square","", "#geometryContainer")
     const size = Math.floor(Math.random()*50) + 100
-    square.style.transform = `rotate(${Math.floor(Math.random()*90)}deg)`
+    square.style.transform = `rotate(${Math.floor(Math.random()*360)}deg)`
     square.style.width = `${size}px`
     square.style.height = `${size}px`
     square.style.position = 'absolute'
     square.style.left = `${Math.floor(Math.random()*(windowWidth-200))}px`
     square.style.top = `${Math.floor(Math.random()*(windowHeight-200))}px`
-    square.innerHTML = `<svg width="${size}" height="${size}">
+    square.innerHTML = `<svg width="${size}" height="${size}" class="shape speed${Math.floor(Math.random()*3) + 2}">
     <defs>
-    <linearGradient id="grad3" x1="0%" y1="0%" x2="100%" y2="0%">
-      <stop offset="0%" style="stop-color: #322991;stop-opacity:1" />
-      <stop offset="100%" style="stop-color: #6827C8;stop-opacity:1" />
-    </linearGradient>
-  </defs>
+        <linearGradient id="grad3" x1="0%" y1="0%" x2="100%" y2="0%">
+        <stop offset="0%" style="stop-color: #322991;stop-opacity:1" />
+        <stop offset="100%" style="stop-color: #6827C8;stop-opacity:1" />
+        </linearGradient>
+    </defs>
   <rect width="${size}" height="${size}" fill="url(#grad3)" style="stroke-width:0;stroke:rgb(0,0,0)" />
 </svg>`
 }
