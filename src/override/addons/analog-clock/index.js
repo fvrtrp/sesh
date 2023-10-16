ec44d711ea89bba712f8a0f8a8b7fca7138450a2
Suraj <surajkukati95@gmail.com>
2023-10-16 19:31:48 +0530

    cleanup clock

--- src/override/addons/analog-clock/index.js
+++ src/override/addons/analog-clock/index.js
@@ -4,29 +4,9 @@ const handWidth = 30, handHeight = 200
 
 export function loadAnalogClock() {
     createElement("dateTimeContainer", "", "#seshParent")
-    let timeContainer = createElement("timeContainer", "", "#dateTimeContainer")
-    // let dateContainer = createElement("dateContainer", "", "#dateTimeContainer")
-    let clock = createElement("clock", "clock", "#dateTimeContainer")
-    // clock.style.width = handHeight*2 + 'px'
-    // clock.style.height = handHeight*2 + 'px'
+    createElement("clock", "clock", "#dateTimeContainer")
     updateDateTime()
-    //setInterval(updateDateTime, 5000)
-}
-
-function getDate() {
-    const date = new Date()
-    const formatted = `${date.getDate()}${nth(date.getDate())} ${date.toLocaleString('default', {month: 'short'})} , ${date.getFullYear()}`
-    return formatted
-
-    function nth(d) {
-        if (d > 3 && d < 21) return 'th'
-        switch (d % 10) {
-          case 1:  return "st"
-          case 2:  return "nd"
-          case 3:  return "rd"
-          default: return "th"
-        }
-    }
+    setInterval(updateDateTime, 5000)
 }
 
 function getTime() {
@@ -37,7 +17,6 @@ function getTime() {
     hours = hours % 12
     hours = hours ? hours : 12 // the hour '0' should be '12'
     minutes = minutes < 10 ? '0'+minutes : minutes
-    let strTime = hours + ':' + minutes + ampm
     return { minutes , hours }
 }
 
@@ -61,10 +40,6 @@ function updateDateTime() {
         clock.style.width = handWidth
         clock.style.height = handHeight
     }
-    // let dateContainer = document.querySelector("#dateContainer")
-    // if(dateContainer) {
-    //     dateContainer.innerHTML = getDate()
-    // }
 }
 
 export function cleanup() {
